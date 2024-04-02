50b3c36b9ff4915605aa3b2c9374c48b95465e5a
javier caceres <javi.54@live.com.ar>
2024-04-02 02:43:06 -0300

    que funque

--- octopush/settings.py
+++ octopush/settings.py
@@ -15,12 +15,17 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rnbme5yl$%=hp3-p$jrnt0lqk7futsu-16ugphno9m70%0_q&@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
