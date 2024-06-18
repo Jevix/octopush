2e44406a9238fdf16b3d76e6d4a2d7509b7c08fd
unknown <nahueluriel2013@gmail.com>
2024-06-18 19:31:35 -0300

    Agregado de archivos Django

--- Gimnasio/proyecto_gym/app_gym/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def IndexView(request):
-    return render(request, 'index.html')
-
-def HomeView(request):
-    return render(request, 'home.html')
