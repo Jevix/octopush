1b40b92e93a4128b79efa082d0a4966e6ea8b331
Jevix <javi.54@live.com.ar>
2024-06-21 15:30:31 -0300

    BackEnd pruebas

--- proyecto_gym/app_gimnasio/views.py
+++ proyecto_gym/app_gimnasio/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.http import HttpResponse
 def index(request):
-    return render(request, 'index.html')
+
+    if request.method == 'POST':
+        username = request.POST.get('inputUser')
+        password = request.POST.get('inputContrasena')
+        if username == 'admin' and password == 'admin':
+            return redirect('/home/')
+        else:
+            return render(request, 'index.html', {'success_message': "Dni no valido o contraseña no valido"})
+    else:
+        return render(request, 'index.html')
+ 
+        
+
+
+
+    
+
+
+    
 
 def home(request):
     return render(request, 'home.html')
